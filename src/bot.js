72fbed95640d18c27e761f3109492711f7757aec
vikornienko <vikornienko76@gmail.com>
2024-11-17 22:06:55 +0500

    Add command for send photo in bot.

--- src/bot.js
+++ src/bot.js
@@ -3,10 +3,26 @@ const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.command('sun', ctx => {
+    bot.telegram.sendChatAction(ctx.chat.id, 'upload_photo');
+    bot.telegram.sendPhoto(ctx.chat.id, {
+      source:
+        "./src/res/sun.jpg",
+    },
+    {
+    reply_to_message_id: ctx.message.message_id
+    });
+});
+bot.command('goodmorning', ctx => {
+    bot.telegram.sendPhoto(ctx.chat.id, {
+        source: "./src/res/goodmorning.jpg"
+    })
+})
+
 bot.launch()
 
 // Enable graceful stop
